17baa4b0ca4c91c4cf679a4e777363d60c58d7f2
Markus Szokoll <markus.szokoll@gmx.at>
2019-01-26 16:58:55 +0100

    output

--- js/index.js
+++ js/index.js
@@ -6,12 +6,13 @@ function calculateInsurance(){
 	Cty = document.getElementById("ccountry").value;
 	HP =  parseInt(document.getElementById("chp").value);
 	Age = parseInt(document.getElementById("cage").value);
+	cName = document.getElementById("cname").value;
 
 	nix = HP * iif(Cty=="Austria",100,iif(Cty=="Hungary",120,150)) / (Age + Number(iif(Cty=="Greece",3,0))) + iif(Cty=="Hungary", 100, 50);
 
 	if(isNaN(nix)){document.getElementById("ccalcResult").innerHTML += "<h3> false input, please input right numbers!!</h3>";}
 	else{
-		document.getElementById("ccalcResult").innerHTML += "<h3>"+(nix.toString() + "<br>")+"</h3>";}
+		document.getElementById("ccalcResult").innerHTML += "<h3>"+ cName +", your insurance costs: " + (nix.toString() + " € <br>")+"</h3>";}
 
 	
 		
